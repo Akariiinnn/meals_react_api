30bada90babe47850bebab3e4daa7c6ef54b53e9
Akariiinnn <robert.quent@gmail.com>
2023-12-11 13:56:20 +0100

    added loading to the meals

--- src/components/AllMeals.jsx
+++ src/components/AllMeals.jsx
@@ -13,20 +13,38 @@ const AllMeals = () => {
             setMeals(await mealsResponse.json());
         })()
     }
+
+    const fakeMealsArray = [];
+
+    for(let i = 0; i < 25; i++)
+    {
+        fakeMealsArray.push({
+            strMeal: "... ",
+            strMealThumb: "/images/loading.gif"
+        })
+    }
+
     return (
         <section>
             <div style={{display: "flex", flexWrap: "wrap"}}>
-            {meals ? (
-                <>
-                    {meals.meals.map((meal) => {
-                        return (
-                            <MealCard meal={meal} style={{width:"20%"}} />
-                        );
-                    })}
-                </>
-            ) : (
-                <p>Loading...</p>
-            )}
+                {meals ? (
+                    <>
+                        {meals.meals.map((meal) => {
+                            return (
+                                <MealCard meal={meal} style={{width: "20%"}}/>
+                            );
+                        })}
+                    </>
+                ) : (
+                    <>
+                        {fakeMealsArray.map((meal) => {
+                            return (
+                                <MealCard meal={meal} style={{width: "20%"}}/>
+                            );
+                        })}
+                    </>
+                )}
+
             </div>
         </section>
     )
